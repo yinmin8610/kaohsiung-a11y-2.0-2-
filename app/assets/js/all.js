342737ea5face2ee56ba7c30670f4c4c8a483cea
yinmin <alice0050722@gmail.com>
2020-10-11 17:29:29 +0800

    首頁＋swiper

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -4,59 +4,20 @@ $(document).ready(() => {
     $(this).toggleClass("show");
     $(this).find(".dropdown-menu").toggleClass("show");
   });
-  //表單無障礙提示
-
-
-  const rewriteBtn = document.getElementById("rewriteBtn");
-  const sendBtn = document.getElementById("sendBtn");
-
-
-  // const form = document.getElementById("form");
-  // const formGroup = document.querySelectorAll("form-group")
-  const name = document.getElementById("inputName");
-  const email = document.getElementById("inputEmail");
-  const tel = document.getElementById("inputTel");
-  const reason = document.getElementById("inputReason");
-  const textarea = document.getElementById("formControlTextarea");
-
-  $(rewriteBtn).on('click', () => {
-    $.each($(this).parent().siblings().find("input"), (i, val) => {
-      val.value = "";
-    });
-    $.each($(this).parent().siblings().find("textarea"), (i, val) => {
-      val.value = "";
-    })
+  //Initialize Swiper 
+  var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
   });
-
-  $(sendBtn).on('click', () => {
-    // console.log($(this).parent().parent())
-    // const formData = $(this).parent().parent().serialize();
-    // console.log(formData);
-    $.ajax({
-      type: 'POST',
-      url: '../contact_form.php',
-      dataType: "json",
-      data: {
-        name: name.val()
-      },
-      success: function (response) {
-        alert(response.success);
-      },
-      error: function (xhr, status, error) {
-        console.log(xhr);
-      }
-    });
-  });
-
-
-  if (name.value === "") {
-    $(name).addClass("is-invalid");
-  } else {
-    $(name).addClass("is-valid");
-  };
-  if (email.value === "") {
-    $(email).addClass("is-invalid");
-  } else {
-    $(email).addClass("is-valid");
-  };
 });
