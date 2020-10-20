14be53ed1edfbc032b444dc627a1f400f64e7dab
Kang-zh <paris267@gmail.com>
2020-10-20 23:11:34 +0800

    修改 硬體設置防治技術首圖、圖片路徑、環保草案法規內容、breadcrumb js

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,38 +3,41 @@ $(document).ready(() => {
   $(".navbar-nav .nav-item").hover(function () {
     $(this).toggleClass("show");
     $(this).find(".dropdown-menu").toggleClass("show");
-  })
+  });
 });
 //宣導專區 - 餐飲的影響 麵包削更換
-if (location.pathname === '/props-influence.html') {
-  const important = document.getElementById('props-important-list');
-  const technology = document.getElementById('props-influences-list');
-  let current = document.getElementById('current-position');
-  important.addEventListener('click', ChangeBurgerMenu, false);
+const important = document.getElementById("props-important-list");
+const technology = document.getElementById("props-influences-list");
+
+if (important !== null && technology !== null) {
+  let current = document.getElementById("current-position");
+  important.addEventListener("click", ChangeBurgerMenu, false);
   technology.addEventListener("click", ChangeBurgerMenu, false);
-  function ChangeBurgerMenu (e) {
+  function ChangeBurgerMenu(e) {
     current.title = e.target.innerHTML;
     current.innerHTML = e.target.innerHTML;
   }
 }
 //宣導專區 -  油煙異味防治 麵包削更換
-if (location.pathname === '/props-prevent.html') {
-  const countermeasure = document.getElementById('props-countermeasure-list');
-  const technology = document.getElementById('props-technology-list');
-  let current = document.getElementById('current-position');
-  countermeasure.addEventListener('click', ChangeBurgerMenu, false);
-  technology.addEventListener('click', ChangeBurgerMenu, false);
-  function ChangeBurgerMenu (e) {
+const countermeasure = document.getElementById("props-countermeasure-list");
+const technology = document.getElementById("props-technology-list");
+
+if (countermeasure !== null && technology !== null) {
+  let current = document.getElementById("current-position");
+  countermeasure.addEventListener("click", ChangeBurgerMenu, false);
+  technology.addEventListener("click", ChangeBurgerMenu, false);
+  function ChangeBurgerMenu(e) {
     current.title = e.target.innerHTML;
     current.innerHTML = e.target.innerHTML;
   }
 }
 //宣導專區 -  環保管制法規 麵包削更換
-if (location.pathname === "/props-laws.html") {
-  const airPollution = document.getElementById("props-airPollution-list");
-  const airPollutionGuidelines = document.getElementById(
-    "props-airPollutionGuidelines-list"
-  );
+const airPollution = document.getElementById("props-airPollution-list");
+const airPollutionGuidelines = document.getElementById(
+  "props-airPollutionGuidelines-list"
+);
+
+if (airPollution !== null && airPollutionGuidelines !== null) {
   const waterPollution = document.getElementById("props-waterPollution-list");
   const waste = document.getElementById("props-waste-list");
   const noise = document.getElementById("props-noise-list");
